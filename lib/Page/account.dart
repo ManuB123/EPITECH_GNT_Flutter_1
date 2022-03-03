ab1339971b858abe94d851df1e090322810fd29a
nielshoez <49568785+nielshoez@users.noreply.github.com>
2022-03-03 22:38:53 +0100

    added recipe list to profile

--- lib/Page/account.dart
+++ lib/Page/account.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:test_flutter/data/userData.dart';
 import 'package:test_flutter/Components/userWidget.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:test_flutter/data/recipe_data.dart';
 
+final List<Widget> imageSliders = Recipe_Lopes.recipes
+    .map((item) => Container(
+  child: Container(
+    margin: const EdgeInsets.all(5.0),
+    child: ClipRRect(
+        borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+        child: Column(
+          children: <Widget>[
+            Flexible(flex: 2, child: Image.network(item.image, fit: BoxFit.cover, width: 1000.0)),
+            Flexible(flex: 1, child:Row( children: [Text(item.name, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold))])),
+            Flexible(flex: 1, child: Row(
+              children: const [
+                Text('4.0', style: TextStyle(color: Colors.deepOrange, fontSize: 18 )),
+                Icon(Icons.star, color: Colors.deepOrange),
+                Icon(Icons.star, color: Colors.deepOrange),
+                Icon(Icons.star, color: Colors.deepOrange),
+                Icon(Icons.star, color: Colors.deepOrange),
+                Icon(Icons.star_border, color: Colors.deepOrange),
+              ],
+            )),
+          ],
+        )),
+  ),
+))
+    .toList();
 class Account extends StatefulWidget {
+  const Account({Key? key}) : super(key: key);
+
   @override
   State<Account> createState() => _AccountState();
 }
-
 class _AccountState extends State<Account> {
   @override
   Widget build(BuildContext context) {
@@ -15,10 +43,10 @@ class _AccountState extends State<Account> {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        title: Text("My Profile", style: TextStyle(color: Colors.black)),
+        title: const Text("My Profile", style: TextStyle(color: Colors.black)),
       ),
       body: ListView(
-        physics: BouncingScrollPhysics(),
+        physics: const BouncingScrollPhysics(),
         children: [
           UserWidget(
             image: user.image,
@@ -36,7 +64,57 @@ class _AccountState extends State<Account> {
                 Text(user.description, style: const TextStyle(fontSize: 16, height: 1.4))
               ],
             ) ,
-          )
+          ),
+          const SizedBox(height: 24,),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 48),
+            child:Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                IntrinsicHeight (
+                child:Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: const [
+                        Text("Recipes"),
+                        Text("3")
+                      ],
+                    ),
+                    VerticalDivider(),
+                    Column(
+                      children: const [
+                        Text("Followers"),
+                        Text("160")
+                      ],
+                    ),
+                    VerticalDivider(),
+                    Column(
+                      children: const [
+                        Text("Following"),
+                        Text("120")
+                      ],
+                    )
+                  ],
+                )
+                ),
+              ],
+            ) ,
+          ),
+          const SizedBox(height: 24),
+          const Divider(thickness: 2),
+          const SizedBox(height: 24),
+          Padding(padding: EdgeInsets.symmetric(horizontal: 48), child:const Text("My Recipes", style: TextStyle(fontSize: 24))),
+          const SizedBox(height: 24),
+          CarouselSlider(
+            options: CarouselOptions(
+              autoPlay: false,
+              aspectRatio: 2.0,
+              enlargeCenterPage: true,
+            ),
+            items: imageSliders,
+          ),
+          const SizedBox(height: 24),
         ],
       )
     );
--- lib/data/recipe_data.dart
+++ lib/data/recipe_data.dart
@@ -4,6 +4,10 @@ class Recipe_Lopes {
   List<String> steps = ["Ajouter du lopes"];
   Map<String, int > ingredients = { "lopes": 10 };
 
-  static const recipe = Recipe(image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.hervecuisine.com%2Fwp-content%2Fuploads%2F2020%2F05%2Frecette-clafoutis-facile-1024x685.jpg.webp&imgrefurl=https%3A%2F%2Fwww.hervecuisine.com%2Frecette%2Frecette-clafoutis-facile-aux-cerises%2F&tbnid=KAvSuvZo04PeWM&vet=12ahUKEwjHwortgIz2AhURxeAKHfqMD8sQMygAegUIARD2AQ..i&docid=vD5yHiZxGlLzAM&w=1024&h=685&q=clafoutis&ved=2ahUKEwjHwortgIz2AhURxeAKHfqMD8sQMygAegUIARD2AQ",
-      author_name: "Alexis lopes", author_picture: "", score: 5, name: "clafoutis", steps: ["Ajouter du lopes"], ingredients: { "lopes": 10 }, description: 'miam');
+  static const recipes = [Recipe(image: "https://pbs.twimg.com/media/Ee6DZChXoAMePMe?format=jpg&name=large",
+      author_name: "Alexis lopes", author_picture: "", score: 5, name: "Indonesian chicken burger", steps: ["Ajouter du lopes"], ingredients: { "lopes": 10 }, description: 'miam'),
+    Recipe(image: "http://wishfulchef.com/wp-content/uploads/2011/10/ButternutSquashSoup.jpg",
+        author_name: "Alexis lopes", author_picture: "", score: 5, name: "Butternut cream soup", steps: ["Ajouter du lopes"], ingredients: { "lopes": 10 }, description: 'miam'),
+    Recipe(image: "https://www.aldi.com.au/fileadmin/fm-dam/images/Recipes/2020/July/ALN3145_WK30_PD_455x315__COOKED-SALMON-POKE-BOWL-RECIPE_.jpg",
+        author_name: "Alexis lopes", author_picture: "", score: 5, name: "Grilled salmon pokebowl", steps: ["Ajouter du lopes"], ingredients: { "lopes": 10 }, description: 'miam')];
 }
