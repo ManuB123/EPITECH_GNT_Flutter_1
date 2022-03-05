64b1ca164bf4d4974f8b8594b4a3534a1487bab0
Laurane Sevin <laurane.sevin@epitech.eu>
2022-03-05 12:44:36 +0100

    fix: styling on account page

--- lib/Components/userWidget.dart
+++ lib/Components/userWidget.dart
@@ -17,15 +17,15 @@ class UserWidget extends StatelessWidget {
     return Column(
       children: [
         const SizedBox(height: 24),
-        Center(child: Text(Profile.user.name, style: const TextStyle(fontSize: 24))),
-        const SizedBox(height: 24),
+        Center(child: Text(Profile.user.name, style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold))),
+        const SizedBox(height: 8),
         Container(
           padding: EdgeInsets.symmetric(horizontal: 48),
           child:Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const SizedBox(height: 24),
-              Text(Profile.user.description, style: const TextStyle(fontSize: 16, height: 1.4))
+              Text(Profile.user.description, style: const TextStyle(fontSize: 16, height: 1.4, color: Color(0xffA1A1A1)))
             ],
           ) ,
         ),
@@ -42,21 +42,21 @@ class UserWidget extends StatelessWidget {
                       Column(
                         children: const [
                           Text("Recipes"),
-                          Text("3")
+                          Text("3", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20))
                         ],
                       ),
                       VerticalDivider(),
                       Column(
                         children: const [
                           Text("Followers"),
-                          Text("160")
+                          Text("160", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20))
                         ],
                       ),
                       VerticalDivider(),
                       Column(
                         children: const [
                           Text("Following"),
-                          Text("120")
+                          Text("120", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20))
                         ],
                       )
                     ],
@@ -68,7 +68,7 @@ class UserWidget extends StatelessWidget {
         const SizedBox(height: 24),
         const Divider(thickness: 2),
         const SizedBox(height: 24),
-        Padding(padding: EdgeInsets.symmetric(horizontal: 48), child:const Text("My Recipes", style: TextStyle(fontSize: 24))),
+        Padding(padding: EdgeInsets.symmetric(horizontal: 48), child:const Text("My Recipes", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold))),
         const SizedBox(height: 24),
         CarouselSlider(
           options: CarouselOptions(
